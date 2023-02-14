a159b09df60aea33b255cb77bc145648f02d69a3
Farid-Abishov <abisovfarid14@gmail.com>
2023-02-15 00:06:49 +0400

    add sections and favicon

--- sandbox/src/js/app.js
+++ sandbox/src/js/app.js
@@ -7,3 +7,26 @@ window.addEventListener("scroll",()=>{
         Navbar.classList.remove("active_nav");
     }
 })
+var swiper = new Swiper(".card-slider", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      breakpoints: {
+        240: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
+      },
+    },
+  });
